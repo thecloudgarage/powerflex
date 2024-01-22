a5dd54eb72b3afbca1799097471ee8fc0a047345
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-01-22 20:35:26 +0530

    Update create-ami-for-ec2-ubuntu-20-04.tf

--- create-ami-for-ec2-ubuntu-20-04.tf
+++ create-ami-for-ec2-ubuntu-20-04.tf
@@ -23,9 +23,10 @@ resource "aws_instance" "sdc" {
   user_data = <<EOF
 #!/bin/bash
 sudo apt update -y
+#INSTALL REQUIRED PACKAGES
 sudo apt install tree libaio1 linux-image-5.4.0-167-generic linux-image-extra-virtual libnuma1 uuid-runtime nano sshpass unzip -y
 cd /home/ubuntu
-
+#RECONFIGURE GRUB TO USE THE COMPATIBLE LINUX KERNEL
 cp /etc/default/grub /etc/default/grub.backup
 # In the above list of packages, we have installed 5.4.0-167-generic linux kernel for ubuntu 20.04
 # This ensures compatability with the scini driver files available in Dell FTP site
@@ -34,6 +35,7 @@ sed -i \
 s/GRUB_DEFAULT=0/GRUB_DEFAULT='"Advanced options for Ubuntu>Ubuntu, with Linux 5.4.0-167-generic"'/g \
 /etc/default/grub
 sudo update-grub
+#DOWNLOAD AND INSTALL POWERFLEX SDC PACKAGES
 cd /home/ubuntu
 wget https://pflex-packages.s3.eu-west-1.amazonaws.com/pflex-45/Software_Only_Complete_4.5.0_287/PowerFlex_4.5.0.287_SDCs_for_manual_install.zip
 unzip PowerFlex_4.5.0.287_SDCs_for_manual_install.zip
@@ -43,6 +45,7 @@ cd PowerFlex_4.5.0.287_Ubuntu20.04_SDC/
 tar -xvf EMC-ScaleIO-sdc-4.5-0.287.Ubuntu.20.04.4.x86_64.tar
 ./siob_extract EMC-ScaleIO-sdc-4.5-0.287.Ubuntu.20.04.4.x86_64.siob
 MDM_IP=${local.mdmIpAddresses} dpkg -i EMC-ScaleIO-sdc-4.5-0.287.Ubuntu.20.04.4.x86_64.deb
+#DOWNLOAD THE COMPATIBLE SCINI.TAR FROM DELL FTP SITE
 export REPO_USER=QNzgdxXix
 export REPO_PASSWORD=Aw3wFAwAq3
 export MDM_IP=${local.mdmIpAddresses}
@@ -51,18 +54,20 @@ sudo source ~/.profile
 cd /bin/emc/scaleio/scini_sync/
 wget https://raw.githubusercontent.com/thecloudgarage/powerflex/main/driver_sync.conf
 touch /etc/emc/scaleio/scini_test.txt
+#INSTALL SCINI SERVICE WITH COMPATIBILITY
 /bin/emc/scaleio/scini_sync/driver_sync.sh scini retrieve Ubuntu20.04/4.5.0.287/5.4.0-167-generic/
 systemctl restart scini
 sleep 20
+#ENSURE UUID IS UNIQUELY CREATED FOR SDC ELSE THERE WILL BE A CONFLICT
 cd /etc/emc/scaleio/
 wget https://raw.githubusercontent.com/thecloudgarage/powerflex/main/set_scini_initiator.sh
-#sudo sed -i "s/mdmIpAddress/\$MDM_IP/g" set_scini_initiator.sh
 chmod +x /etc/emc/scaleio/set_scini_initiator.sh
 cd /etc/systemd/system/
 wget https://raw.githubusercontent.com/thecloudgarage/powerflex/main/set_scini_initiator.service
 sudo systemctl daemon-reload
 sudo systemctl enable set_scini_initiator.service
 sudo systemctl start set_scini_initiator.service
+#ENSURE PASSWORD BASED AUTH IS ENABLED
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sudo service ssh restart
