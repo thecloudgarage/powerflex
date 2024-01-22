1f4afb561960eda6ea680f3add6dbc3b8713c64d
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2024-01-22 13:15:02 +0530

    Update set_scini_initiator.sh

--- set_scini_initiator.sh
+++ set_scini_initiator.sh
@@ -3,9 +3,10 @@ if ls -al /etc/emc/scaleio | grep scini_test.txt; then
 systemctl restart scini
 exit
 else
+mdmIP=replaceMdmIp
 echo -e "test" > /etc/emc/scaleio/scini_test.txt
 export uuid=$(uuidgen)
-echo -e "ini_guid $uuid\nmdm ${MDM_IP}" > /etc/emc/scaleio/drv_cfg.txt
+echo -e "ini_guid $uuid\nmdm ${replaceMdmIp}" > /etc/emc/scaleio/drv_cfg.txt
 sleep 10
 systemctl restart scini
 fi
